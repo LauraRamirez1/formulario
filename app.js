86b562697eca023d6a3817cda9abe11dc68a773d
LauraRamirezAltera <laura.r.altera@gmail.com>
2020-09-03 22:09:37 -0400

    validaciones 1

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-      name: null,
-      email: null,
-      subject: null,
-      message: null,
-    }
+var app= new Vue({
+    el:'#app',
+    data(){
+        return{
+            name: null,
+            email: null,
+            subject: null,
+            message: null,
+            errors: []
+        }
+    },
+    methods: {
+        checkForm: function (e) {
+            this.errors = [];
+            if (!this.name) {
+                        this.errors.push('El nombre es obligatorio.');
+                    } 
+            if (!this.email) {
+                        this.errors.push('El correo electrónico es obligatorio.');
+                    }
+            if (!this.subject) {
+                        this.errors.push('El asunto es obligatorio.');
+                    }
+            if (!this.message) {
+                        this.errors.push('El mensaje es obligatorio.');
+                    }
+            if (!this.errors.length) {
+            return true;
+            }
+            e.preventDefault();
+        }
+    },
 })
